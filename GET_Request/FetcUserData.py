d228d45a2d2320972e2db1d87d114132b6e683f9
Vidhya <guru.vidhyab@gmail.com>
2020-11-18 22:39:13 +0000

    Adding all files into Repo

---
+++ GET_Request/FetcUserData.py
@@ -0,0 +1,6 @@
+import requests
+
+url = "https://reqres.in/api/users?page=2"
+
+response = requests.get(url)
+print(response)
---
+++ TestCases/test_CreateUser.py
@@ -0,0 +1,64 @@
+import requests
+import pytest
+import yaml
+
+
+class TestSet_Users(object):
+    """
+    Test Set for testing users functionality of https://gorest.co.in
+    """
+    format = "json"
+
+    url = "https://gorest.co.in/public-api/users"
+
+    def setup_method(self):
+        settingsFile = open("C:/Users/Babukumar V/PycharmProjects/RESTAPI1/TestCases/Test_Settings.yaml", "r")
+        setting = yaml.safe_load(settingsFile)
+        self.acc_token = setting["api_key"]
+
+    def __find_string_in_response(self, fullResponse, searchFor):
+        check = True
+        rawResponse = fullResponse
+        if "data" not in rawResponse.text:
+            check = False
+        else:
+            responseJSON = rawResponse.json()
+            length_responseJSON = len(responseJSON["data"])
+            for i in range(0,length_responseJSON,1):
+                check =  searchFor in responseJSON["data"][i]["name"]
+                print('The value is ',length_responseJSON,searchFor,i)
+                if check == False:
+                    return check
+        return check
+
+    def test_Create(self):
+
+        payload = {"name": "firstuser1","email": "Firstuser1@testonly.com","gender": "Male","status": "Active"}
+        headers = {
+                'Authorization': 'Bearer ' + self.acc_token,
+                'Content-Type': 'application/json'
+              }
+
+        response = requests.post(self.url, headers=headers, json = payload)
+
+        print('Test is executing')
+
+        print(response.text.encode('utf8'))
+
+    # Test-2
+    @pytest.mark.parametrize("name", ["firstuser1", "firstuser", "Oscar"])
+    def test_get_a_user(self, name):
+        payload = {"name": name}
+        headers = {
+            'Authorization': 'Bearer ' + self.acc_token,
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.get(self.url, headers=headers, json=payload)
+
+        print('Test is executing')
+
+        print(response.text.encode('utf8'))
+
+        check = self.__find_string_in_response(response, name)
+        assert check == True
