e3d6f0e0e7f65ba1905c31f74ead907c061bafa5
Vidhya <guru.vidhyab@gmail.com>
2020-11-19 16:15:11 +0000

    Modified test cases to check for fail

--- TestCases/test_CreateUser.py
+++ TestCases/test_CreateUser.py
@@ -15,6 +15,7 @@ class TestSet_Users(object):
         settingsFile = open("Test_Settings.yaml", "r")
         setting = yaml.safe_load(settingsFile)
         self.acc_token = setting["api_key"]
+        print(self.acc_token)
 
     def __find_string_in_response(self, fullResponse, searchFor):
         check = True
@@ -24,16 +25,19 @@ class TestSet_Users(object):
         else:
             responseJSON = rawResponse.json()
             length_responseJSON = len(responseJSON["data"])
-            for i in range(0,length_responseJSON,1):
-                check =  searchFor in responseJSON["data"][i]["name"]
-                print('The value is ',length_responseJSON,searchFor,i)
-                if check == False:
-                    return check
+            if length_responseJSON == 0:
+                check = False
+                for i in range(0,length_responseJSON,1):
+                    check =  searchFor in responseJSON["data"][i]["name"]
+                    print('The value is ',length_responseJSON,searchFor,i)
+                    if check == False:
+                        return check
+            return check
         return check
 
     def test_Create(self):
 
-        payload = {"name": "firstuser3","email": "Firstuser3@testonly.com","gender": "Male","status": "Active"}
+        payload = {"name": "revathi","email": "Firstuser1232@testonly.com","gender": "Male","status": "Active"}
         headers = {
                 'Authorization': 'Bearer ' + self.acc_token,
                 'Content-Type': 'application/json'
@@ -42,17 +46,20 @@ class TestSet_Users(object):
         response = requests.post(self.url, headers=headers, json = payload)
 
         print('Test is executing')
+        print(response.status_code)
+        
 
         print(response.text.encode('utf8'))
 
     # Test-2
-    @pytest.mark.parametrize("name", ["firstuser2", "firstuser", "Oscar"])
+    @pytest.mark.parametrize("name", ["zzfirstuser3", "firstuser1", "revathi"])
     def test_get_a_user(self, name):
         payload = {"name": name}
         headers = {
             'Authorization': 'Bearer ' + self.acc_token,
             'Content-Type': 'application/json'
         }
+        print(headers, self.url)
 
         response = requests.get(self.url, headers=headers, json=payload)
 
@@ -60,5 +67,8 @@ class TestSet_Users(object):
 
         print(response.text.encode('utf8'))
 
+        print(name)
+
         check = self.__find_string_in_response(response, name)
-        assert check == True, "GEt API Test failed"
+
+        assert check == True
